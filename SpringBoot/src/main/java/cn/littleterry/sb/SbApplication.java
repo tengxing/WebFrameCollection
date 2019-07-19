876b44d3f15a7f754e0606dc756f1d724bf5e9b9
terry <tengxing7452@163.com>
2019-07-19 08:29:16 +0800

    SpringBoot

--- SpringBoot/src/main/java/cn/littleterry/sb/SbApplication.java
+++ SpringBoot/src/main/java/cn/littleterry/sb/SbApplication.java
@@ -5,7 +5,6 @@ import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
