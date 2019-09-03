6405ef756b43e81fb50bd5fa9b3db7de3c8cd58d
terry <tengxing7452@163.com>
2019-09-03 23:28:48 +0800

    SpringBootMybatis-Simple

--- SpringBootMybatis-Simple/src/main/java/cn/littleterry/sb/CityMapper.java
+++ SpringBootMybatis-Simple/src/main/java/cn/littleterry/sb/CityMapper.java
@@ -1,5 +1,6 @@
 package cn.littleterry.sb;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
@@ -13,5 +14,11 @@ public interface CityMapper {
 
     @Select("select id, name, state, country from city where state = #{state}")
     City findByState(@Param("state") String state);
+
+    @Insert("insert into city values(#{city.id},#{city.name},#{city.state},#{city.country})")
+    int insert(@Param("city") City city);
+
+    @Insert("insert into city values(#{id},#{name},#{state},#{country})")
+    int insertSimple(City city);
 
 }
